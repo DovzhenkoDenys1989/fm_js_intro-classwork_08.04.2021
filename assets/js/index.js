fdf9c7065854b929ecc5c5d90777c85ad47e911a
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-11 13:01:40 +0300

    copy from the last classwork 07.04.2021

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+'use strict';
+// str.split(' ').join(' ')
+
+/* 
+  Для каждого слова сделать первую буквы большой.
+  String.prototype.split
+  String.prototype.toUpperCase
+  Array.prototype.join
+*/
+const str = "some string i need";
+
+const capitalize = (str) => str.split(" ").map((word) => word[0].toUpperCase() + word.slice(1)).join(" ");
+
+console.log(capitalize(str));
